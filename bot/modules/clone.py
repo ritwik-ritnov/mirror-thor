8913855e132c3c88605d66cc493a9a92bbd1154e
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2021-11-21 06:52:36 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -67,6 +67,7 @@ def cloneNode(update, context):
             sendMessage(men + result, context.bot, update)
         else:
             sendMarkup(result + cc, context.bot, update, button)
+            postCH(result + cc, context.bot, update, button)
     else:
         sendMessage('𝐏𝐫𝐨𝐯𝐢𝐝𝐞 𝐆-𝐃𝐫𝐢𝐯𝐞 𝐒𝐡𝐚𝐫𝐞𝐚𝐛𝐥𝐞 𝐋𝐢𝐧𝐤 𝐭𝐨 𝐂𝐥𝐨𝐧𝐞.', context.bot, update)
 

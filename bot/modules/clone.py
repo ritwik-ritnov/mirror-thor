e0287636da638960875b278df890601bd12bdfb8
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2021-11-21 06:29:43 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -61,7 +61,7 @@ def cloneNode(update, context):
         else:
             uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
         if uname is not None:
-            cc = f'\nby: {uname}'
+            cc = f'\n<b>╰─👤by</b>: {uname}'
             men = f'{uname} '
         if button in ["cancelled", ""]:
             sendMessage(men + result, context.bot, update)
